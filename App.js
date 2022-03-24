16bafbacfa46ea0655e49d512035cf33c99c9b81
Won Ju Oh <reopard506@gmail.com>
2022-03-24 09:28:08 +0900

    modules

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+export default function App() {
+  const [isSignIn, setSignIn] = useState(false);
+
+  return (
+    <>
+      {/* <ApolloProvider client={client}> */}
+      <AppearanceProvider>
+        <ThemeProvider theme={Theme}>
+          <NavigationContainer>
+            {isSignIn ? <SignInNav /> : <SignOutNav />}
+          </NavigationContainer>
+        </ThemeProvider>
+      </AppearanceProvider>
+      {/* </ApolloProvider> */}
+    </>
+  );
+}
